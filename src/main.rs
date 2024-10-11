0ca45952b22b27295dc473420d77939dacd5b19d
Hernán Valdés <hfvaldesg@gmail.com>
2024-10-11 18:01:54 +0200

    Remove mapping of message. Return Task::none for uuid generator

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,8 @@ impl DevTools {
         match event {
             Message::UuidGenerator(message) => {
                 if let Screen::UuidGenerator(uuid_generator) = &mut self.screen {
-                    uuid_generator.update(message).map(Message::UuidGenerator)
+                    uuid_generator.update(message);
+                    Task::none()
                 } else {
                     Task::none()
                 }
