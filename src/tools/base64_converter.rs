d6593b5288d5e5f670e39227bcb7d9abf22a7ed5
Hernán Valdés <hfvaldesg@gmail.com>
2024-10-18 17:31:43 +0200

    Add controls and basic messages for encode and decode options

--- src/tools/base64_converter.rs
+++ src/tools/base64_converter.rs
@@ -1,15 +1,28 @@
 use iced::{
-    widget::{horizontal_space, row, text},
+    widget::{column, container, text_input},
     Element, Length,
 };
 
 pub const NAME: &str = "Base64 Converter";
 
 #[derive(Debug, Clone)]
-pub enum Message {}
+pub enum Message {
+    DecodedTextChanged(String),
+    EncodedTextChanged(String),
+}
+
+#[derive(Debug, Clone, Default)]
+enum Mode {
+    #[default]
+    Encode,
+    Decode,
+}
 
 pub struct Base64Converter {
     tool_name: String,
+    encoded_text: String,
+    decoded_text: String,
+    active_mode: Mode,
 }
 
 impl Default for Base64Converter {
@@ -22,6 +35,9 @@ impl Base64Converter {
     pub fn new() -> Self {
         Self {
             tool_name: NAME.to_string(),
+            encoded_text: String::new(),
+            decoded_text: String::new(),
+            active_mode: Mode::default(),
         }
     }
 
@@ -30,8 +46,19 @@ impl Base64Converter {
     }
 
     pub fn view(&self) -> Element<Message> {
-        row![text("dummy"), horizontal_space()]
-            .height(Length::Fill)
-            .into()
+        let controls = column![
+            "Decoded",
+            text_input("", self.decoded_text.as_str()).on_input_maybe(match self.active_mode {
+                Mode::Encode => Some(Message::DecodedTextChanged),
+                Mode::Decode => None,
+            }),
+            "Encoded",
+            text_input("", self.encoded_text.as_str()).on_input_maybe(match self.active_mode {
+                Mode::Encode => None,
+                Mode::Decode => Some(Message::EncodedTextChanged),
+            }),
+        ];
+        let content = container(controls).padding(10).height(Length::Fill);
+        content.into()
     }
 }
